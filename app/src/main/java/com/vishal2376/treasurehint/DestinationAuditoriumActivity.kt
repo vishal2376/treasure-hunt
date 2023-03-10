943b8f31beb8ef722407b5e18507c7074f68c27c
Vishal & Singh <vishalsingh2376@gmail.com>
2023-03-10 23:09:29 +0530

    UI : auditorium destination activity

---
+++ app/src/main/java/com/vishal2376/treasurehint/DestinationAuditoriumActivity.kt
@@ -0,0 +1,11 @@
+package com.vishal2376.treasurehint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DestinationAuditoriumActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_destination_auditorium)
+    }
+}
