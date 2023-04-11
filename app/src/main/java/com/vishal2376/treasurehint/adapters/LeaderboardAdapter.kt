c9a510beb67fc32814be02f9da04eba8bfb9f781
Aditya Sood <adityasood314@gmail.com>
2023-04-12 00:36:45 +0530

    Updated Leaderboard

--- app/src/main/java/com/vishal2376/treasurehint/adapters/LeaderboardAdapter.kt
+++ app/src/main/java/com/vishal2376/treasurehint/adapters/LeaderboardAdapter.kt
@@ -9,16 +9,18 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.vishal2376.treasurehint.R
 import com.vishal2376.treasurehint.models.LeaderBoard
 import com.vishal2376.treasurehint.models.LeaderboardModel
+import com.vishal2376.treasurehint.models.Team
 
-class LeaderboardAdapter(val leaderboard:LeaderBoard):RecyclerView.Adapter<LeaderboardAdapter.ViewHolder>() {
+class LeaderboardAdapter(val list: List<Team>):RecyclerView.Adapter<LeaderboardAdapter.ViewHolder>() {
     class ViewHolder(view: View):RecyclerView.ViewHolder(view){
         val tvScore:TextView
         val tvName:TextView
+        val tvPosition:TextView
         init {
             tvName = view.findViewById(R.id.tvName)
             tvScore = view.findViewById(R.id.tvScore)
+            tvPosition = view.findViewById(R.id.tvPosition)
         }
-
 
     }
 
@@ -28,15 +30,15 @@ class LeaderboardAdapter(val leaderboard:LeaderBoard):RecyclerView.Adapter<Leade
     }
 
     override fun getItemCount(): Int {
-        return leaderboard.teams.size
+        return list.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        val sortedTeams=leaderboard.teams.sortedByDescending{
 //
-//        }
-        holder.tvName.text = leaderboard.teams[position].name
-        holder.tvScore.text = leaderboard.teams[position].score.toString()
+//        val rank = position + 4
+//        holder.tvPosition.text = rank.toString()
+        holder.tvName.text = list[position].name
+        holder.tvScore.text = list[position].score.toString()
     }
 
 }
