735317a052a047c792069557a1a904a1f6051ca3
Aditya Sood <adityasood314@gmail.com>
2023-04-12 18:32:19 +0530

    Updated Leaderboard activity

--- app/src/main/java/com/vishal2376/treasurehint/LeaderboardActivity.kt
+++ app/src/main/java/com/vishal2376/treasurehint/LeaderboardActivity.kt
@@ -34,43 +34,7 @@ class LeaderboardActivity : AppCompatActivity() {
         leaderboardArray.add(leaderboardModel)
         binding.rvLeaderboard.layoutManager=LinearLayoutManager(this)
         viewModel.allStatus.observe(this, Observer {
-            val list=viewModel.leaderBoard.value?.teams?.sortedByDescending {it.score}
-            Log.i("adi ", "size is ${list?.size} ")
-            val size = list?.size
-            binding.tvYourTeam.text = user.team.name
-            binding.tvYourScore.text = user.team.score.toString()
-            Log.d("Network","${user.team}")
-//            val index = list!!.indexOfFirst{
-//                it.name == user.team.name
-//            }
-//            Log.i("adi", "$index")
-//
-//            binding.tvYourPosition.text = index.toString()
 
-
-//            for(i in 0 until size!!){
-//                var index =0
-//                if(list[i].name == user.team.name){
-//                   index = i+1
-//                }
-//            }
-
-            if (size != null) {
-                when (size) {
-                    1 -> {
-                        binding.tvFirst.text = list[0].name
-                    }
-                    2 -> {
-                        binding.tvFirst.text = list[0].name
-                        binding.tvSecond.text = list[1].name
-                    }
-                    else -> {
-                        binding.tvFirst.text = list[0].name
-                        binding.tvSecond.text = list[1].name
-                        binding.tvThird.text = list[2].name
-                    }
-                }
-            }
           when(viewModel.allStatus.value)
           {
               ApiStatus.LOADING->{}
@@ -79,6 +43,43 @@ class LeaderboardActivity : AppCompatActivity() {
               }
 
               ApiStatus.SUCCESS->{
+                  val list=viewModel.leaderBoard.value?.teams?.sortedByDescending {it.score}
+                  Log.i("adi ", "size is ${list?.size} ")
+                  val size = list?.size
+                  binding.tvYourName.text = user.team.name
+                  binding.tvYourScore.text = user.team.score.toString()
+                  Log.d("Network","${user.team}")
+                  val index = list!!.indexOfFirst{
+                      it.name == user.team.name
+                  }
+                  Log.i("adi", "$index")
+
+                  binding.tvYourPosition.text = index.toString()
+
+
+                  if (size != null) {
+                      when (size) {
+                          1 -> {
+                              binding.tvFirst.text = list[0].name
+                              binding.tvFirstScore.text = list[0].score.toString()
+                          }
+                          2 -> {
+                              binding.tvFirst.text = list[0].name
+                              binding.tvFirstScore.text = list[0].score.toString()
+                              binding.tvSecond.text = list[1].name
+                              binding.tvSecondScore.text = list[1].score.toString()
+                          }
+                          else -> {
+                              binding.tvFirst.text = list[0].name
+                              binding.tvFirstScore.text = list[0].score.toString()
+                              binding.tvSecond.text = list[1].name
+                              binding.tvSecondScore.text = list[1].score.toString()
+                              binding.tvThird.text = list[2].name
+                              binding.tvThirdScore.text = list[2].score.toString()
+
+                          }
+                      }
+                  }
                   if (size != null) {
                       if(size>3){
                           binding.rvLeaderboard.adapter = LeaderboardAdapter(list)
