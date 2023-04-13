40ab4d0e1945840b4d2c69a67313e9fa6f670cd1
SATVIK SHARMA <sat091201@gmail.com>
2023-04-13 17:06:53 +0530

    Update: layouts updated

--- app/src/main/java/com/vishal2376/treasurehint/util/Constants.kt
+++ app/src/main/java/com/vishal2376/treasurehint/util/Constants.kt
@@ -7,10 +7,10 @@ object Constants {
     var Locations = mutableListOf<Int>(2, 1, 4, 5, 3)
     val Checkpoints = Locations.filter { it != 1 }.toTypedArray()
     val LocationNames = mapOf(
-        1 to "4H",
-        2 to "Auditorium",
-        3 to "Ground",
-        4 to "Open Air Gym",
-        5 to "SAC"
+        1 to "SAC",
+        2 to "Open Air Gym",
+        3 to "4H",
+        4 to "OAT",
+        5 to "Auditorium"
     )
 }
