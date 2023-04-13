773cdb84124d28ce97ad30f6dd2987e5a62aaaa1
Aditya Sood <adityasood314@gmail.com>
2023-04-13 17:47:26 +0530

    Update: Updated layouts and created SP activity

---
+++ app/src/main/java/com/vishal2376/treasurehint/locations/DestinationSPActivity.kt
@@ -0,0 +1,12 @@
+package com.vishal2376.treasurehint.locations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vishal2376.treasurehint.R
+
+class DestinationSPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_destination_spactivity)
+    }
+}
